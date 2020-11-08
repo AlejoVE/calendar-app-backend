08762f598c7a450feec65f99633377e39d166397
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2020-11-08 17:32:59 +0100

    changes in DB connection, try 2

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection =  async () => {
      try {
         // mongoose.connect('process.env.DB_CNN', {useNewUrlParser: true, useUnifiedTopology: true});
-        await mongoose.connect('process.env.DB_CNN', {
+        await mongoose.connect(process.env.DB_CNN, {
             useNewUrlParser: true, 
             useUnifiedTopology: true,
             useCreateIndex: true
